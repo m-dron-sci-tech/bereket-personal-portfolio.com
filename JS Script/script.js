6f3196e76871bffa89ed029fa9c5c06e1cc1e093
M Dron Sci-Tech <162838812+m-dron-sci-tech@users.noreply.github.com>
2024-03-10 00:26:22 +0300

    Add files via upload

---
+++ JS Script/script.js
@@ -0,0 +1,51 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing",{
+	strings:["","Student","Web Designer","Web Developer","Graphic Designer","Gamer","Hacker"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-first",{
+	strings:["","Website type :-","","Website with a login form",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-first-2nd",{
+	strings:["","More Detail about :-","","This is my first home made website and it have best login form",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-Second",{
+	strings:["","More Detail about :-","","2nd HomeMade Website",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-Second-2nd",{
+	strings:["","More Detail about :-","","This is my Second home made website ",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+
+
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-third",{
+	strings:["","More Detail about :-","","beautiful Login Form",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing-third-2nd",{
+	strings:["","More Detail about :-","","This is my first login form without website",],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
