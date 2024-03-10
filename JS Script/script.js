9f80329c26cdfcab8d0eab0818157045e50348d4
M Dron Sci-Tech <162838812+m-dron-sci-tech@users.noreply.github.com>
2024-03-10 03:21:50 +0300

    Update script.js

--- JS Script/script.js
+++ JS Script/script.js
@@ -1,6 +1,6 @@
 /* ========================== typing animation ========================== */
 var typed = new Typed(".typing",{
-	strings:["","Student","Graphic Designer","Programmer","","Front-End-Developer","Ethical Hacker","UI/UX Designer",""],
+	strings:[,"Student","Graphic Designer","Programmer",,"Front-End-Developer","Ethical Hacker","UI/UX Designer",],
 	typeSpeed:100,
 	BackSpeed:60,
 	loop:true
